db7b910ca37039df628939ec3b3401264d6b33e7
udattab2 <udattab2@gmail.com>
2022-07-04 19:26:31 -0500

    Added Python3 Compatibility
    Changed Tracks.py

--- tracks.py
+++ tracks.py
@@ -6,17 +6,17 @@ def track_browse(nm4, flag, cur):
 	while True:
 		os.system('cls')
 		if flag==1: break
-		print '===============',nm4[0],'===============\n\n'
-		print 'Year of Release: ', nm4[2],'\n\n'
-		print 'No. of Tracks: ',nm4[1],'\n\n'
-		print 'Tracklist: \n\n'
+		print('===============',nm4[0],'===============\n\n')
+		print('Year of Release: ', nm4[2],'\n\n')
+		print('No. of Tracks: ',nm4[1],'\n\n')
+		print('Tracklist: \n\n')
 		cur.execute('''select song_no, name from track where album_id=?''',(nm4[3], ))
 		nm5=cur.fetchall()
 		for trackname in nm5:
-			print trackname[0],'->',trackname[1],'\n\n'
-		print '\n\nb-> Back\n\n'
-		print 'm-> Main\n\n'
-		ch3= raw_input('Enter Choice: ')
+			print(trackname[0],'->',trackname[1],'\n\n')
+		print('\n\nb-> Back\n\n')
+		print('m-> Main\n\n')
+		ch3= input('Enter Choice: ')
 		if ch3=='b':
 			break
 		elif ch3=='m':
@@ -31,12 +31,12 @@ def track_src(trackres, flag, cur):
 	while True:
 		os.system('cls')
 		if flag==1: break
-		print '===============SEARCH RESULTS===============\n\n'
+		print('===============SEARCH RESULTS===============\n\n')
 		for trackname in trackres:
-			print trackname[0],'->',trackname[1],'\n\n'
-		print '\n\nb-> Back\n\n'
-		print 'm-> Main\n\n'
-		ch3= raw_input('Enter Choice: ')
+			print(trackname[0],'->',trackname[1],'\n\n')
+		print('\n\nb-> Back\n\n')
+		print('m-> Main\n\n')
+		ch3= input('Enter Choice: ')
 		if ch3=='b':
 			break
 		elif ch3=='m':
